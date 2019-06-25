ead113eb2b42c7aee8d4af4e6896010e6aa80038
R_Abderhalden <reto88@gmx.net>
2019-06-25 14:08:47 +0200

    Anpassungen Reto

--- Quellcode/hfupilot/hfupilot.getsp/hfucontext.Designer.cs
+++ Quellcode/hfupilot/hfupilot.getsp/hfucontext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Michael\HFU\Projektwoche-2019\Quellcode\hfupilot\hfupilot.getsp\hfucontext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\reto8\OneDrive\Desktop\NDS\Pr-Woche\Quellcode\hfupilot\hfupilot.getsp\hfucontext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
