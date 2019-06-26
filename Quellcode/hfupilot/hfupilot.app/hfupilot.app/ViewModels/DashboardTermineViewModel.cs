31b0e6acf42ffcdad11d3682919534a6ce834ae5
R_Abderhalden <reto88@gmx.net>
2019-06-26 13:00:26 +0200

    Dashboard Erweiterung

--- Quellcode/hfupilot/hfupilot.app/hfupilot.app/ViewModels/DashboardTermineViewModel.cs
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app/ViewModels/DashboardTermineViewModel.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using hfupilot.app.CustomFramework.mvvm;
 
 namespace hfupilot.app.ViewModels
 {
-    class DashboardTermineViewModel:ObservableObject
+    class DashboardTermineViewModel
     {
+
+        #region Felder
+
+        public ObservableCollection<TermineViewModel> Termine { get; set; }
+
+        #endregion
+
+        #region Konstruktor
+
+        public DashboardTermineViewModel()
+        {
+            Termine = new ObservableCollection<TermineViewModel>();
+
+            aktualisiereTermine();
+        }
+
+        #endregion
+
+        private void aktualisiereTermine()
+        {
+            Termine.Clear();
+            //Termine.Add()
+        }
+
+
+
+
     }
 }
---
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app/ViewModels/TermineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using hfupilot.app.CustomFramework.mvvm;
+
+namespace hfupilot.app.ViewModels
+{
+    class TermineViewModel : ObservableObject
+    {
+        public DateTime Datum;
+    }
+}
