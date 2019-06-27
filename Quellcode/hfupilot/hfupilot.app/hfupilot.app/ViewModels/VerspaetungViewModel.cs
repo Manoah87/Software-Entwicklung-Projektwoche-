323de2a33160d0edd33ad023656825b37f69ade3
GitNik1 <nikiriedle@hotmail.com>
2019-06-27 14:21:13 +0200

    wip
    wip
    
    
    wip

---
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app/ViewModels/VerspaetungViewModel.cs
@@ -0,0 +1,53 @@
+﻿using hfupilot.app.CustomFramework.mvvm;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using hfupilot.app.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using hfupilot.app.models.api;
+using System.Threading.Tasks;
+using hfupilot.app.Models;
+
+namespace hfupilot.app.ViewModels
+{
+    class VerspaetungViewModel : ObservableObject
+    {
+        private List<int> _verspeatungsAuswahl;
+        private string _begruendung;
+        private readonly INavigation _navigation;
+        private readonly IViewMapper _viewMapper;
+        private readonly HttpClient _httpClient;
+        public ICommand SpeicherCommand { get; set; }
+
+        public VerspaetungViewModel()
+        {
+            _verspeatungsAuswahl = new List<int> { 1, 2, 3, 4, 5 };
+        }
+
+        public List<int> Verspeatungen
+        {
+            get { return _verspeatungsAuswahl; }
+            set
+            {
+                if (_verspeatungsAuswahl != value)
+                {
+                    _verspeatungsAuswahl = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Begruendung
+        {
+            get => _begruendung;
+            set
+            {
+                _begruendung = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
