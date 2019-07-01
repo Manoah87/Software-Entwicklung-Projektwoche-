7724873506f0b1937aebb33acf58d1bdecd9024b
R_Abderhalden <reto88@gmx.net>
2019-07-01 19:32:50 +0200

    Biometric - Setup Step 3+4

---
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app/IBiometricAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hfupilot.app
+{
+    public interface IBiometricAuthenticationService
+    {
+        void Authenticate(Action success, Action<string> onError);
+    }
+}
