a0c23600b5105b078ea057a24db30d568d2a8c27
GitNik1 <nikiriedle@hotmail.com>
2019-07-02 23:34:55 +0200

    notification works

---
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app.Android/FirebaseService.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Util;
+using Firebase.Iid;
+
+namespace hfupilot.app.Droid
+{
+  [Service]
+  [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+  public class MyFirebaseIIDService : FirebaseInstanceIdService
+  {
+    const string TAG = "FirebaseInit";
+
+    public override void OnTokenRefresh()
+    {
+      var refreshedToken = FirebaseInstanceId.Instance.Token;
+      Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+      SendRegistrationToServer(refreshedToken);
+    }
+
+    public void SendRegistrationToServer(string token)
+    {
+      // Add custom implementation, as needed.
+    }
+  }
+}
--- Quellcode/hfupilot/hfupilot.app/hfupilot.app.Android/MainActivity.cs
+++ Quellcode/hfupilot/hfupilot.app/hfupilot.app.Android/MainActivity.cs
@@ -7,6 +7,19 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+
+using Android.Content;
+using Xamarin.Forms;
+using hfupilot.app.ViewModels;
+using hfupilot.app.Services;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
+
 namespace hfupilot.app.Droid
 {
     [Activity(Label = "hfupilot.app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -39,6 +52,77 @@ namespace hfupilot.app.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    {
+        public MainActivity()
+        {
+            Activity = this;
+        }
+
+        public static Activity Activity { get; private set; }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            if (!Forms.IsInitialized)
+            {
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
+
+                base.OnCreate(savedInstanceState);
+
+                Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+                Forms.Init(this, savedInstanceState);
+                LoadApplication(new App());
+            }
+            else
+            {
+                base.OnCreate(savedInstanceState);
+            }
+
+            // Check if our notification was clicked while the app was closed.
+            var extra = Intent.GetBooleanExtra("FromNotification", false);
+            if (extra)
+            {
+                CheckForListRequest();
+            }
+            else if (Intent.Extras?.Get("RemoteKey") != null)
+            {
+                CheckForListRequest();
+            }
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            // Do something with the data you pass from the notification.
+            var extra = intent.GetBooleanExtra("FromNotification", false);
+            if (extra)
+            {
+                CheckForListRequest();
+            }
+
+            base.OnNewIntent(intent);
+        }
+
+        private void CheckForListRequest()
+        {
+            RunOnUiThread(async () =>
+            {
+                // Navigate to another view if the notification is clicked.
+                var viewModel = App.Services.GetInstance<AnmeldenViewModel>();
+                var viewMapper = App.Services.GetInstance<IViewMapper>();
+
+                // Add a wait to make sure our MainView is loaded up.
+                await Task.Delay(200);
+                await App.Services.GetInstance<INavigation>().PushAsync(viewMapper.Map(viewModel));
+            });
         }
     }
 }
